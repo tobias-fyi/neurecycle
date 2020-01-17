4299c6046ab295ec5ae6954b4e92a589c4548722
tobias-fyi <tobyreaper@gmail.com>
2020-01-17 16:57:01 -0700

    YOLOv3 - attempt #1 with Keras and TensorFlow

---
+++ notebooks/draw_boxes.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+
+
+def draw_boxes(filename, v_boxes, v_labels, v_scores):
+    """Draws all results."""
+
+    # load the image
+    data = plt.imread(filename)
+    # plot the image
+    plt.imshow(data)
+    # get the context for drawing boxes
+    ax = plt.gca()
+    # plot each box
+    for i in range(len(v_boxes)):
+        box = v_boxes[i]
+        # get coordinates
+        y1, x1, y2, x2 = box.ymin, box.xmin, box.ymax, box.xmax
+        # calculate width and height of the box
+        width, height = x2 - x1, y2 - y1
+        # create the shape
+        rect = Rectangle((x1, y1), width, height, fill=False, color="white")
+        # draw the box
+        ax.add_patch(rect)
+        # draw text and score in top left corner
+        label = "%s (%.3f)" % (v_labels[i], v_scores[i])
+        plt.text(x1, y1, label, color="white")
+    # show the plot
+    plt.show()
